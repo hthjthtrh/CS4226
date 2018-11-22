9d61fb4b09c94057a6b57f9d8e1de60ed118cb47
Renxing Liu <liurenxing1993@gmail.com>
2018-11-22 20:48:36 +0800

    Add files via upload

--- controller.py
+++ controller.py
@@ -17,6 +17,7 @@ from pox.lib.revent import *
 from pox.lib.util import dpid_to_str
 from pox.lib.addresses import IPAddr, EthAddr
 from pox.lib.packet import ethernet
+from pox.lib.recoco import Timer
 
 log = core.getLogger()
 
@@ -25,90 +26,113 @@ class Controller(EventMixin):
         self.listenTo(core.openflow)
         core.openflow_discovery.addListeners(self)
         self.macMap = {}
-        self.flooded = {}
+        self.reset_timer = Timer(5, self.reset_mac_map, recurring=True)
+
+    def reset_mac_map(self):
+        for switch in self.macMap:
+            self.macMap[switch].clear()
         
         
     # You can write other functions as you need.
         
     def _handle_PacketIn (self, event):    
         # install entries to the route table
-
-        packet = event.parsed
-        dpid = event.dpid
-        ofMsg = event.ofp
-        inPort = event.port
-        src_mac = packet.src
-        dst_mac = packet.dst
-
-        # map the src_mac to the port number, but keep the earliest mapping
-        if not mac_port_known(dpid, src_mac):
-            self.macMap[dpid][src_mac] = inPort
-        
-        if mac_port_known(dpid, dst_mac):
-            dest_port = self.macMap[dpid][dst_mac]
-            forward()
-            installFlowEntries()
-        else:
-            if not switch_flooded(dpid, mac):
-                flood()
-                self.flooded[dpid][mac] = True
+        #print(self.macMap)
 
         def mac_port_known(dpid, mac):
-            switch_macs = self.macMap.get(dpid)
-            return switch_macs.get(mac) != None
+            return self.macMap[dpid].get(mac) != None
 
-        def switch_flooded(dpid, mac):
-            return self.flooded[dpid].get(mac) != None
         
         def install_enqueue(event, packet, outport, q_id):
+            a = 1
           
 
         # Check the packet and decide how to route the packet
         def forward(message = None):
-            log.debug("Forwarding for packet of source {}, destination {}".format(src_mac, dst_mac))
+            log.debug("\nForwarding for packet of source h{}, destination h{}".format(src_mac, dst_mac))
             msg = of.ofp_packet_out()
             msg.data = ofMsg
-            msg.actions.append(of.ofp_action_output(port = dest_port))
+            msg.actions.append(of.ofp_action_output(port = self.macMap[dpid][dst_mac]))
             event.connection.send(msg)
 
         # install the flow entry
         def installFlowEntries(message = None):
-            log.debug("Installing flow entries for hosts {}, {}".format(src_mac, dst_mac))
+            log.debug("\nInstalling flow entries for hosts h{}, h{}".format(src_mac, dst_mac))
             msg = of.ofp_flow_mod()
+            msg.match.dl_src = src_mac
             msg.match.dl_dst = dst_mac
-            msg.actions.append(of.ofp_action_output(port = dest_port))
+            msg.hard_timeout = 5
+            msg.actions.append(of.ofp_action_output(port = self.macMap[dpid][dst_mac]))
+            event.connection.send(msg)
 
             nxt_msg = of.ofp_flow_mod()
+            nxt_msg.match.dl_src = dst_mac
             nxt_msg.match.dl_dst = src_mac
-            nxt_msg.actions.append(of.ofp_action_output(port = inPort))
-
-            event.connection.send(msg)
+            nxt_msg.hard_timeout = 5
+            nxt_msg.actions.append(of.ofp_action_output(port = self.macMap[dpid][src_mac]))            
             event.connection.send(nxt_msg)
 
 
         # When it knows nothing about the destination, flood but don't install the rule
         def flood (message = None):
-            log.debug("Flooding for packet of source {}".format(src_mac))
+            log.debug("\nFlooding for packet of source h{}".format(src_mac))
             msg = of.ofp_packet_out()
             msg.data = ofMsg
-            msg.actions.append(of.ofp_action_output(port = of.OFPP_ALL)
+            msg.actions.append(of.ofp_action_output(port = of.OFPP_ALL))
             event.connection.send(msg)
-    
+
+
+        packet = event.parsed
+        dpid = dpid_to_str(event.dpid)
+        ofMsg = event.ofp
+        inPort = event.port
+        src_mac = packet.src
+        dst_mac = packet.dst
+        payload = packet.payload
+        src_ip = None
+        dst_ip = None
+        if packet.type == ethernet.IP_TYPE:
+            src_ip = payload.srcip
+            dst_ip = payload.dstip
+        elif packet.type == ethernet.ARP_TYPE:
+            src_ip = payload.protosrc
+            dst_ip = payload.protodst
+        '''
+        print("switch: {}".format(dpid))
+        print("src mac: {}, dest mac : {}".format(src_mac, dst_mac))
+        print("src ip: {}, dest ip : {}".format(src_ip, dst_ip))
+        '''
+        # map the src_mac to the port number, but keep the earliest mapping
+        if not mac_port_known(dpid, src_mac):
+            self.macMap[dpid][src_mac] = inPort
+
+        if dst_mac.is_multicast or (not mac_port_known(dpid, dst_mac)):
+            flood()
+        else:
+            installFlowEntries()
+            forward()  
+
+        #print("\n")
+
 
 
     def _handle_ConnectionUp(self, event):
         dpid = dpid_to_str(event.dpid)
         log.debug("Switch %s has come up.", dpid)
         self.macMap[dpid] = {}
-        self.flooded[dpid] = {}
         
         # Send the firewall policies to the switch
+        '''
         def sendFirewallPolicy(connection, policy):
             
 
         for i in [FIREWALL POLICIES]:
             sendFirewallPolicy(event.connection, i)
-            
+        '''
+    
+    def _handle_ConnectionDown(self, event):
+        dpid = dpid_to_str(event.dpid)
+        log.debug("Switch %s has gone down.", dpid)
 
 def launch():
     # Run discovery and spanning tree modules
