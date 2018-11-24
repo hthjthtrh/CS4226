5b243d59f23ca20da03c3ba31232dd3e36ea9786
Renxing Liu <liurenxing1993@gmail.com>
2018-11-24 22:51:29 +0800

    Update mininetTopo.py

--- mininetTopo.py
+++ mininetTopo.py
@@ -71,7 +71,7 @@ def startNetwork():
 
     global net
     net = Mininet(topo=topo, link = TCLink,
-                  controller=lambda name: RemoteController(name, ip='127.0.0.1'),
+                  controller=lambda name: RemoteController(name, ip='192.168.56.101'),
                   listenPort=6633, autoSetMacs=True)
 
     info('** Starting the network\n')
